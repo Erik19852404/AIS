2a20566246ba013f574a1e2c1f406d0b88d39444
Erik Apyshev <yorick_1985@mail.ru>
2020-11-27 17:57:01 +0600

    Add methods for getting and check elements

--- TestCasesLogin.py
+++ TestCasesLogin.py
@@ -19,12 +19,26 @@ import Global
 
 webSiteAddress = 'http://192.168.13.213/'
 maxWaitTimeSec = 5
-
+ByParamsForGetElement = {
+    'byXPath' : 'find_element_by_xpath',
+    'byCSS' : "find_element_by_css_selector",
+    'byClass' : 'find_element_by_class_name',
+    'byID' : 'find_element_by_id',
+    'byName' : 'find_element_by_name',
+    'byText' : 'find_element_by_partial_link_text',
+    'byTag' : 'find_element_by_tag_name'
+}
 loginPass = {
     'correctLogin' : 'user_sp',
-    'correctPassword' : '12345',
     'emptyLogin' : '',
+    'wrongUserLogin' : 'user_wrong',
+    'correctPassword' : '12345',
     'emptyPassword' : ''
+}
+testResult = {
+    "Unknown" : "Test was not run...",
+    "Success" : "Test was complete successfully...",
+    "Failed" : "Test failed..."
 }
 
 class TestCaseAuthorization(unittest.TestCase):    
@@ -35,6 +49,9 @@ class TestCaseAuthorization(unittest.TestCase):
     def tearDown(self):#some actions after test run
         self.driver.close()
 
+    def doModuleCleanups():#called unconditionally after test
+        pass
+
     def test_OpenMainPage(self):
         driver = self.driver
         driver.get(webSiteAddress)
@@ -43,26 +60,56 @@ class TestCaseAuthorization(unittest.TestCase):
     def test_LoginUser(self):
         driver = self.driver
         driver.get(webSiteAddress)
-        self.Login(loginPass['correctLogin'], loginPass['correctPassword'])
-        WebDriverWait(driver, maxWaitTimeSec).until(EC.presence_of_element_located((By.XPATH,'//*[@id="appMenu"]/a[1]')))
-        WebDriverWait(driver, maxWaitTimeSec).until(EC.presence_of_element_located((By.XPATH,'//*[@id="content"]/div[4]/a[1]')))
-        WebDriverWait(driver, maxWaitTimeSec).until(EC.visibility_of_element_located((By.CLASS_NAME, 'Logout')))
+        self.assertEqual(self.Login(loginPass['correctLogin'], loginPass['correctPassword']), testResult['Success'], 'Login')
+        self.CheckElementPresence(By.XPATH,'//*[@id="appMenu"]/a[1]', "Menu")
+        self.CheckElementPresence(By.XPATH,'//*[@id="content"]/div[4]/a[1]', "Content container")
+        self.CheckElementPresence(By.CLASS_NAME, 'Logout', "Logout button")
 
     def test_LoginEmpty(self):
         driver = self.driver
         driver.get(webSiteAddress)
         self.Login(loginPass['emptyLogin'], loginPass['emptyPassword'])
-        WebDriverWait(driver, maxWaitTimeSec).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]'))
+        self.CheckElementPresence(By.XPATH, '/html/body/div[3]', "Wrong login message window")
 
+    def test_LoginWrongUser(self):
+        driver = self.driver
+        driver.get(webSiteAddress)
+        self.Login(loginPass['wrongUserLogin'], loginPass['emptyPassword'])
+        self.CheckElementPresence(By.XPATH, '/html/body/div[3]', "Wrong login message window")
 
     def Login(self, userLogin, userPassword):
+        result = testResult['Unknown']
+        login = self.GetElement('byXPath', '//*[@id="username2"]', 'Login field')
+        password = self.GetElement('byXPath', '//*[@id="password"]', 'Password field')
+        btnEnter = self.GetElement('byXPath', '//*[@id="login"]', 'Confirm button')
+        if login != None and password != None and btnEnter != None:
+            login.send_keys(userLogin)
+            password.send_keys(userPassword)
+            btnEnter.click()
+            result = testResult['Success']
+        else:
+            result = testResult['Failed']
+        return result
+
+    def CheckElementPresence(self, ByParams, elementIdentificator, elementName):
         driver = self.driver
-        login = driver.find_element_by_xpath('//*[@id="username"]')
-        password = driver.find_element_by_xpath('//*[@id="password"]')
-        btnEnter = driver.find_element_by_xpath('//*[@id="login"]')
-        login.send_keys(userLogin)
-        password.send_keys(userPassword)
-        btnEnter.click()
+        try:
+            WebDriverWait(driver, maxWaitTimeSec).until(EC.presence_of_element_located((ByParams, elementIdentificator)))
+        except Exception as ex:
+            print("No presence of element '" + elementName + "'...")
+            logging.error(ex)
+
+    def GetElement(self, ByParamsStr, elementIdentificator, elementName):
+        driver = self.driver
+        element = None
+        try:
+            getElementFunc = getattr(driver, ByParamsForGetElement[ByParamsStr])
+            element = getElementFunc(elementIdentificator) 
+        except Exception as ex:
+            print("Can't get element '" + elementName + "'...")
+            logging.error(ex)
+        return element
+
         
 if __name__ == '__main__':
     unittest.main()
